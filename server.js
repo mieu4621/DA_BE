816cdfd2a5b7815278724343ca0bc983cc9e1b8c
ThanhBlack273 <62133884+ThanhBlack273@users.noreply.github.com>
2022-10-20 22:17:46 +0700

    update npm

--- server.js
+++ server.js
@@ -322,40 +322,33 @@ mongoClient.connect(url, (err, db) =>{
       })
 
       // Upload ảnh
-      app.post('/uploadimg',upload.single('image'), async function(req,res){
-         const upImg = await cloudinary.uploader.upload(req.file.path) //up anh len cloudinary
+      app.post('/uploadimg', upload.single('image'), async function(req,res){
+        const upImg = await cloudinary.uploader.upload(req.file.path) //up anh len cloudinary
+       
+        const myDb = db.db('test')
+        const collection = myDb.collection('Users')
+        // up anh len mongo
+        const ava= { $set:{avatar: upImg.url, cloudinary_id: upImg.public_id}}
+        const query = { email: req.body.email }
+        collection.findOne(query, async (err, result) => {
+           if (result!=null) {
+             if(result.avatar != "")
+             {
+               await cloudinary.uploader.destroy(result.cloudinary_id)
+             }
+               collection.updateOne(query, ava, function(err, result){
+                 if (!err) res.status(200).send();
+               })
+            } 
+            else if (result==null){
+              res.status(400).send()
+            } 
+            else res.status(404).send()
+
+          })
         
-         const myDb = db.db('test')
-         const collection = myDb.collection('Users')
-         // up anh len mongo
-         const ava= { $set:{avatar: upImg.url, cloudinary_id: upImg.public_id}}
-         const query = { email: req.body.email }
-         collection.findOne(query, async (err, result) => {
-            if (result!=null) {
-              if(result.avatar != "")
-              {
-                await cloudinary.uploader.destroy(result.cloudinary_id)
-              }
-                collection.updateOne(query, ava, function(err, result){
-                  if (!err) res.status(200).send();
-                })
-             } 
-             else if (result==null){
-               res.status(400).send()
-             } 
-             else res.status(404).send()
- 
-           })
-         
-      })
-
-
-
-      
-      
-
-      
-    }
+     })
+   }
     
 });
 
